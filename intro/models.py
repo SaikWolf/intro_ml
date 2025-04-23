3c948530e4334ea5d7c5db2fb05deac33e73c0fc
Bill Clark <saikou@vt.edu>
2025-04-22 23:05:40 -0400

    Focusing in on supervised learning problems

--- intro/models.py
+++ intro/models.py
@@ -1,6 +1,8 @@
 
+import torch
 from torch import nn,optim
 import lightning as lp
+import numpy as np
 
 class Encoder_v0(nn.Module):
     def __init__(self,initial=28*28,steps=[64,3],activation=nn.ReLU()):
@@ -111,3 +113,132 @@ class AutoEncoder_v0(lp.LightningModule):
     def configure_optimizers(self):
         optimizer = self.optim_maker(self.parameters())
         return optimizer
+
+
+
+class Backbone_v0(nn.Module):
+    def __init__(self,initial=(28,28),steps=[3,13,3],activation=nn.ReLU()):
+        # initial shape (H,W)->C=1 or (C,H,W)
+        assert(len(steps)>0)
+        super().__init__()
+        if isinstance(activation,str) and hasattr(nn,activation):
+            activation = getattr(nn,activation)()
+        self.hyperparams = {"initial":initial,"steps":steps,"activation":activation.__class__.__name__}
+        self.in_size = initial if len(initial) > 2 else (1,)+tuple(initial)
+        self.conv_layers = nn.ModuleList([
+            nn.Conv2d(in_channels=self.in_size[0],
+                      out_channels=2*self.in_size[0],
+                      kernel_size=(steps[0],steps[0]),
+                      stride=1,
+                      bias=True)
+        ])
+        self.conv_layers.extend([
+            nn.Conv2d(in_channels=self.in_size[0]*2**(idx),
+                      out_channels=self.in_size[0]*2**(idx+1),
+                      kernel_size=(steps[idx],steps[idx]),
+                      stride=1,
+                      bias=True) for idx in range(1,len(steps))
+        ])
+        self._steps = len(steps)
+        self._looped = self._steps-1
+        self.activation = activation
+        self.out_size = (
+            self.in_size[0]*2**(self._steps),
+            self.in_size[1] - sum([x-1 for x in steps]),
+            self.in_size[2] - sum([x-1 for x in steps]))
+
+    def forward(self, x):
+        for idx in range(self._looped):
+            x = self.activation(self.conv_layers[idx](x))
+        return self.conv_layers[-1](x)
+
+
+class DecisionHead_v0(nn.Module):
+    def __init__(self,initial=(8,12,12),steps=[256,10],activation=nn.ReLU()):
+        # initial shape (H,W)->C=1 or (C,H,W)
+        assert(len(steps)>0)
+        super().__init__()
+        if isinstance(activation,str) and hasattr(nn,activation):
+            activation = getattr(nn,activation)()
+        self.hyperparams = {"initial":initial,"steps":steps,"activation":activation.__class__.__name__}
+        self.in_size = initial if len(initial) > 2 else (1,)+tuple(initial)
+        self.linear_layers = nn.ModuleList([
+            nn.Linear(np.prod(self.in_size),steps[0],bias=True)
+        ])
+        self.linear_layers.extend([
+            nn.Linear(steps[idx],steps[idx+1],bias=True) for idx in range(len(steps)-1)
+        ])
+        self._steps = len(steps)
+        self._looped = self._steps-1
+        self.activation = activation
+        self.out_size = steps[-1]
+
+    def forward(self, x):
+        for idx in range(self._looped):
+            x = self.activation(self.linear_layers[idx](x))
+        return self.linear_layers[-1](x)
+
+
+class Classifier_v0(lp.LightningModule):
+    def __init__(self,
+                 backbone=Backbone_v0(),
+                 head=DecisionHead_v0(),
+                 loss_fcn=nn.functional.cross_entropy,
+                 optim_fcn=lambda params: optim.Adam(params,lr=1e-3)
+                ):
+        super().__init__()
+        if isinstance(backbone,dict):
+            backbone = Backbone_v0(**backbone)
+        else:
+            self.save_hyperparameters({"backbone":backbone.hyperparams})
+        if isinstance(head,dict):
+            head = DecisionHead_v0(**head)
+        else:
+            self.save_hyperparameters({"head":head.hyperparams})
+        self.backbone = backbone
+        self.head = head
+        self.criterion = loss_fcn
+        self.optim_maker = optim_fcn
+
+    def training_step(self, batch, batch_idx):
+        observation, label_or_target = batch
+
+        # size(0) -> batch size, so flatten observation in a vector
+        x = self.backbone(observation)
+        y = x.view(observation.size(0),-1)
+        z = self.head(y)
+        loss = self.criterion(z,label_or_target)
+        l_hat = torch.argmax(z,1)
+        acc = torch.eq(l_hat,label_or_target).float().mean()
+        self.log_dict({'train_loss':loss,"train_acc":acc},prog_bar=True)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        observation, label_or_target = batch
+
+        # size(0) -> batch size, so flatten observation in a vector
+        x = self.backbone(observation)
+        y = x.view(observation.size(0),-1)
+        z = self.head(y)
+        loss = self.criterion(z,label_or_target)
+        l_hat = torch.argmax(z,1)
+        acc = torch.eq(l_hat,label_or_target).float().mean()
+        self.log_dict({'val_loss':loss,"val_acc":acc},prog_bar=True)
+    
+    def test_step(self, batch, batch_idx):
+        observation, label_or_target = batch
+
+        # size(0) -> batch size, so flatten observation in a vector
+        x = self.backbone(observation)
+        y = x.view(observation.size(0),-1)
+        z = self.head(y)
+        loss = self.criterion(z,label_or_target)
+        l_hat = torch.argmax(z,1)
+        acc = torch.eq(l_hat,label_or_target).float().mean()
+        self.log_dict({'test_loss':loss,"test_acc":acc})
+
+
+    def configure_optimizers(self):
+        optimizer = self.optim_maker(self.parameters())
+        return optimizer
+
---
+++ scripts/train_infer_fmnist.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+
+try:
+    import intro
+except ModuleNotFoundError:
+    import os,sys
+    sys.path.insert(0,os.path.join(os.environ['VIRTUAL_ENV'],'src','intro_dev'))
+    sys.path.insert(0,os.path.join(os.environ['VIRTUAL_ENV'],'src','intro_ml'))
+    import intro
+from torchvision.datasets import FashionMNIST as MNIST
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from torch.utils import data as datahandling
+import lightning as lp
+
+import argparse
+
+from intro.loggers import LoggerLearning as LL
+
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument('stage',default=0,type=int,help='Select teaching stage to run')
+    p.add_argument('--batch',default=200,type=int,help="Specify the batch size to use (def: %(default)s)")
+    p.add_argument('--save',action='store_true',help='Save off the final model as a checkpoint')
+    return p.parse_args()
+
+
+
+################################################
+## Does it train?
+################################################
+def stage0(batchsize:int=200,pathfile:str=None):
+    dataset = MNIST(intro.get_dataset_dir(),transform=transforms.ToTensor())
+    dataloader = DataLoader(dataset,num_workers=7,batch_size=batchsize)
+
+    decider = intro.models.Classifier_v0()
+
+    logg = LL('cls_fmnist',"stage_0")
+    ## disable auto checkpointing for learning purposes
+    trainer = lp.Trainer(max_epochs=10,accelerator='auto',enable_checkpointing=False,logger=logg)
+    trainer.fit(decider,dataloader)
+    if pathfile is not None:
+        trainer.save_checkpoint(os.path.join(trainer.logger.log_dir,'model.ckpt'))
+
+    trainer.test(decider,dataloader)
+
+
+################################################
+## Was the result above biased?
+################################################
+def stage1(batchsize:int=200,pathfile:str=None):
+    dataset = MNIST(intro.get_dataset_dir(),transform=transforms.ToTensor(), train=True)
+    dataloader = DataLoader(dataset,num_workers=7,batch_size=batchsize)
+
+    decider = intro.models.Classifier_v0()
+
+    logg = LL('cls_fmnist',"stage_1")
+    ## disable auto checkpointing for learning purposes
+    trainer = lp.Trainer(max_epochs=10,accelerator='auto',logger=logg,enable_checkpointing=False)
+    trainer.fit(decider,dataloader)
+    if pathfile is not None:
+        trainer.save_checkpoint(os.path.join(trainer.logger.log_dir,'model.ckpt'))
+
+    testset = MNIST(intro.get_dataset_dir(),transform=transforms.ToTensor(), train=False)
+    testloader = DataLoader(testset,num_workers=7,batch_size=batchsize)
+
+    trainer.test(decider,testloader)
+
+################################################
+## Can we better understand data bias?
+################################################
+def stage2(batchsize:int=200,pathfile:str=None):
+    dataset = MNIST(intro.get_dataset_dir(),transform=transforms.ToTensor())
+
+    train_data, valid_data = datahandling.random_split(dataset,[int(len(dataset)*0.8),len(dataset)-int(len(dataset)*0.8)])
+    dataloader = DataLoader(train_data,num_workers=7,batch_size=batchsize)
+    validloader = DataLoader(valid_data,num_workers=7,batch_size=batchsize)
+    decider = intro.models.Classifier_v0()
+
+    logg = LL('cls_fmnist',"stage_2")
+    ## disable auto checkpointing for learning purposes
+    trainer = lp.Trainer(max_epochs=10,accelerator='auto',logger=logg,enable_checkpointing=False)
+    trainer.fit(decider,dataloader,validloader)
+    if pathfile is not None:
+        trainer.save_checkpoint(os.path.join(trainer.logger.log_dir,'model.ckpt'))
+
+    testset = MNIST(intro.get_dataset_dir(),transform=transforms.ToTensor(), train=False)
+    testloader = DataLoader(testset,num_workers=7,batch_size=batchsize)
+
+    trainer.test(decider,testloader)
+
+################################################
+## Can we automate these bias search?
+################################################
+def stage3(batchsize:int=200,pathfile:str=None):
+    from lightning.pytorch.callbacks.early_stopping import EarlyStopping
+    from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
+
+    dataset = MNIST(intro.get_dataset_dir(),transform=transforms.ToTensor())
+
+    train_data, valid_data = datahandling.random_split(dataset,[int(len(dataset)*0.8),len(dataset)-int(len(dataset)*0.8)])
+    dataloader = DataLoader(train_data,num_workers=7,batch_size=batchsize)
+    validloader = DataLoader(valid_data,num_workers=7,batch_size=batchsize)
+    decider = intro.models.Classifier_v0()
+
+    enable_ckpt = True
+    early_stopper = EarlyStopping(monitor='val_loss',mode='min',patience=3,min_delta=0.001)
+    checkpointer = ModelCheckpoint(monitor='val_loss',mode='min')
+    callbacks = [checkpointer,early_stopper]
+    logg = LL('cls_fmnist',"stage_3")
+    trainer = lp.Trainer(max_epochs=30,accelerator='auto',callbacks=callbacks,enable_checkpointing=enable_ckpt,logger=logg)
+    trainer.fit(decider,dataloader,validloader)
+    decider = intro.models.Classifier_v0.load_from_checkpoint(checkpointer.best_model_path)
+    if pathfile is not None:
+        trainer.save_checkpoint(os.path.join(trainer.logger.log_dir,'model.ckpt'))
+
+    testset = MNIST(intro.get_dataset_dir(),transform=transforms.ToTensor(), train=False)
+    testloader = DataLoader(testset,num_workers=7,batch_size=batchsize)
+
+    trainer.test(decider,testloader)
+
+
+def main():
+    args = parse_args()
+    if args.stage == 0:
+        stage0(args.batch,args.save)
+    elif args.stage == 1:
+        stage1(args.batch,args.save)
+    elif args.stage == 2:
+        stage2(args.batch,args.save)
+    elif args.stage == 3:
+        stage3(args.batch,args.save)
+        
+
+if __name__ == '__main__':
+    main()
